372f65fd0f40ed3e516b0a5bc535135d1f2e1a6b
Sanghoon <shpusan001@gmail.com>
2021-08-25 18:22:13 +0900

    fix SockDto for adding obj(Any)
    내용: SockDto에 오브젝트를 넣을 수 있게 수정

--- src/main/kotlin/dekilla/core/domain/SockDto.kt
+++ src/main/kotlin/dekilla/core/domain/SockDto.kt
@@ -5,5 +5,6 @@ import java.io.Serializable
 data class SockDto(
     val command: String,
     val seperator: String,
-    val data: String
+    val data: String,
+    val obj: Any
 ) : Serializable
